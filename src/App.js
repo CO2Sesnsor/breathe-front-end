a3c95f61a9e386fd10344bf493c41b41ef341bc6
Hasin Mahmood <hasinmahmood01@gmail.com>
2022-10-19 22:33:21 +0100

    made coming soon page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import "./App.css";
+import construction from "./assets/construction.svg";
 
 function App() {
-  return <div>Hello</div>;
+  return (
+    <div className="center">
+      <h1>COMING SOON</h1>
+      <div className="scale-construction-img">
+        <img src={construction} alt="site under construction" />
+      </div>
+    </div>
+  );
 }
 
 export default App;
