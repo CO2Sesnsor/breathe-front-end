131919a968f5337da92126144eb24bce545c4143
Hasin Mahmood <hasinmahmood01@gmail.com>
2022-11-10 21:08:21 +0000

    dump data to chart route

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,17 @@
 import "./App.css";
 // import construction from "./assets/construction.svg";
 
-import Home from "./home";
 import Postingcard from "./posting-card";
 import Cards from "./home";
 import { Route, Routes } from "react-router-dom";
+import Chart from "./chart";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Cards />} />
       <Route path="/postdata" element={<Postingcard />} />
+      <Route path="/chart" element={<Chart />} />
     </Routes>
   );
 }
