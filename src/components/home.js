8bdf15abd6720220c1e568ba11294d0b7e6e63f8
Hasin Mahmood <hasinmahmood01@gmail.com>
2022-11-10 22:35:44 +0000

    remove useref because it wasn't being use

--- src/components/home.js
+++ src/components/home.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect } from "react";
 import sensorReadingsAPI from "../api/sensorReadings";
 import DataCard from "./DataCard";
 import Nav from "./nav";
