ae9380432d33bbaa1446a6b0d3bf7d2a5973471a
Hasin Mahmood <hasinmahmood01@gmail.com>
2022-12-16 20:47:57 +0000

    made new arr for just timestamps in chart component

--- src/components/chart.js
+++ src/components/chart.js
@@ -3,11 +3,20 @@ import { useState, useEffect } from "react";
 const Chart = ({ readings }) => {
   const [co2Values, setCo2Values] = useState([]);
   const [vocValues, setVocValues] = useState([]);
+  const [timeValues, setTimeValues] = useState([]);
   useEffect(() => {
     let co2 = readings.map((current) => current["co2"]);
     let voc = readings.map((current) => current["voc"]);
+    let time = readings.map((current) => {
+      let currentTimeStamp = new Date(current["created_at"]);
+      let hours = currentTimeStamp.getHours();
+      let minutes = currentTimeStamp.getMinutes();
+      let seconds = currentTimeStamp.getSeconds();
+      return `${hours}:${minutes}:${seconds}`;
+    });
     setCo2Values(co2);
     setVocValues(voc);
+    setTimeValues(time);
   }, [JSON.stringify(readings)]);
   useEffect(() => {
     // console.log(co2Values);
@@ -20,6 +29,9 @@ const Chart = ({ readings }) => {
       <p>{co2Values.length}</p>
       <h1>{JSON.stringify(vocValues)}</h1>
       <p>{vocValues.length}</p>
+      <h1>{JSON.stringify(timeValues)}</h1>
+      <p>{timeValues.length}</p>
+      <h1>{JSON.stringify(readings)}</h1>
     </div>
   );
 };
