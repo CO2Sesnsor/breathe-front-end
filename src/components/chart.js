057f6806fdb5176d014cb8dbb67428003ab46442
Hasin Mahmood <hasinmahmood01@gmail.com>
2022-12-16 20:02:57 +0000

    made new arr for just voc values in chart component

--- src/components/chart.js
+++ src/components/chart.js
@@ -2,16 +2,26 @@ import { useState, useEffect } from "react";
 
 const Chart = ({ readings }) => {
   const [co2Values, setCo2Values] = useState([]);
+  const [vocValues, setVocValues] = useState([]);
   useEffect(() => {
-    let co2 = readings.map((a) => a["co2"]);
+    let co2 = readings.map((current) => current["co2"]);
+    let voc = readings.map((current) => current["voc"]);
     setCo2Values(co2);
+    setVocValues(voc);
   }, [JSON.stringify(readings)]);
   useEffect(() => {
     // console.log(co2Values);
     console.log(readings);
   }, [co2Values]);
 
-  return <h1>{JSON.stringify(co2Values)}</h1>;
+  return (
+    <div>
+      <h1>{JSON.stringify(co2Values)}</h1>
+      <p>{co2Values.length}</p>
+      <h1>{JSON.stringify(vocValues)}</h1>
+      <p>{vocValues.length}</p>
+    </div>
+  );
 };
 
 export default Chart;
