4ad5b6958a8d9a9583bf104de5a716deda8ba200
Hasin Mahmood <hasinmahmood01@gmail.com>
2022-12-16 15:16:16 +0000

    only get whole table on mount

--- src/components/chart.js
+++ src/components/chart.js
@@ -1,27 +1,7 @@
 import { useState, useEffect } from "react";
-import sensorReadingsAPI from "../api/sensorReadings";
 
 const Chart = () => {
-  const [reading, setReading] = useState();
-  const setData = () => {
-    const getData = async () => {
-      const data = await sensorReadingsAPI.getReadings();
-      // console.log("making request", data);
-      setReading(data);
-    };
-    getData();
-  };
-  useEffect(() => {
-    const myInterval = setInterval(() => {
-      setData();
-    }, 1000);
-
-    // Clear side-effect when component unmount (componentWillUnmount)
-    return () => {
-      clearInterval(myInterval);
-    };
-  }, []);
-  return <h1>{JSON.stringify(reading)}</h1>;
+  return <h1>hello</h1>;
 };
 
 export default Chart;
