0b4e831a1bd2c23db7e9d754024459fd5e2e8436
Hasin Mahmood <hasinmahmood01@gmail.com>
2022-12-21 16:43:22 +0000

    updated axis labels

--- src/components/chart.js
+++ src/components/chart.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { Line } from "react-chartjs-2";
+import zoomPlugin from "chartjs-plugin-zoom";
 import {
   Chart as ChartJS,
   LineElement,
@@ -9,13 +10,16 @@ import {
   Legend,
 } from "chart.js";
 
-ChartJS.register(LineElement, CategoryScale, LinearScale, PointElement, Legend);
+ChartJS.register(
+  LineElement,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  Legend,
+  zoomPlugin
+);
 
 const Chart = ({ readings, timeInterval, averageInterval, maxPoints }) => {
-  let co2AverageArr = [];
-  let vocAverageArr = [];
-  let timeAverageArr = [];
-  let readingsInOneMin = 40;
   const [co2Values, setCo2Values] = useState([]);
   const [vocValues, setVocValues] = useState([]);
   const [timeValues, setTimeValues] = useState([]);
@@ -38,10 +42,6 @@ const Chart = ({ readings, timeInterval, averageInterval, maxPoints }) => {
   };
 
   useEffect(() => {
-    // console.log(`Interval: ${timeInterval}`);
-    // console.log(`Avg: ${averageInterval}`);
-    // console.log(`Max: ${maxPoints}`);
-
     const readingsCopy = [...readings];
     while (readingsCopy.length > maxPoints) {
       readingsCopy.shift();
@@ -64,6 +64,7 @@ const Chart = ({ readings, timeInterval, averageInterval, maxPoints }) => {
         (e, i) => i % averageInterval === averageInterval - 1
       );
     }
+    console.log(timeArr);
 
     setCo2Values(co2Arr);
     setVocValues(vocArr);
@@ -73,6 +74,55 @@ const Chart = ({ readings, timeInterval, averageInterval, maxPoints }) => {
   // useEffect(() => {
   //   console.log(readings);
   // }, [co2Values]);
+
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "bottom",
+      },
+      zoom: {
+        zoom: {
+          wheel: {
+            enabled: true,
+            speed: 0.05,
+          },
+          pinch: {
+            enabled: true,
+          },
+          mode: "xy",
+        },
+        pan: {
+          enabled: true,
+        },
+      },
+    },
+
+    scales: {
+      PPM: {
+        title: {
+          text: "CO₂ (PPM)",
+          display: true,
+          padding: 0,
+          margin: 0,
+        },
+        beginAtZero: true,
+        type: "linear",
+        position: "right",
+      },
+      PPB: {
+        title: {
+          text: "VOC (PPB)",
+          display: true,
+          padding: 0,
+          margin: 0,
+        },
+        beginAtZero: true,
+        type: "linear",
+        position: "left",
+      },
+    },
+  };
 
   const data = {
     labels: timeValues,
@@ -86,19 +136,19 @@ const Chart = ({ readings, timeInterval, averageInterval, maxPoints }) => {
         borderColor: "#fda4af",
         borderCapStyle: "round",
         tension: 0.4,
-        // pointRadius: (ctx) => {
-        //   const pointsLength = ctx.chart.data.labels.length - 1;
-        //   const pointsArray = [];
+        pointRadius: (ctx) => {
+          const pointsLength = ctx.chart.data.labels.length - 1;
+          const pointsArray = [];
 
-        //   for (let i = 0; i <= pointsLength; i++) {
-        //     if (i === pointsLength) {
-        //       pointsArray.push(3);
-        //     } else {
-        //       pointsArray.push(0);
-        //     }
-        //   }
-        //   return pointsArray;
-        // },
+          for (let i = 0; i <= pointsLength; i++) {
+            if (i === pointsLength) {
+              pointsArray.push(3);
+            } else {
+              pointsArray.push(0);
+            }
+          }
+          return pointsArray;
+        },
         pointBackgroundColor: "rgba(255, 99, 132, 1)",
         yAxisID: "PPM",
       },
@@ -128,38 +178,8 @@ const Chart = ({ readings, timeInterval, averageInterval, maxPoints }) => {
     ],
   };
 
-  const options = {
-    responsive: true,
-    plugins: {
-      legend: {
-        position: "bottom",
-      },
-    },
-    scales: {
-      PPM: {
-        // title: {
-        //   text: "PPM",
-        //   display: true,
-        //   padding: 0,
-        //   margin: 0,
-        // },
-        beginAtZero: true,
-        type: "linear",
-        position: "right",
-      },
-    },
-  };
-
   return (
     <div>
-      {/* <h1>{JSON.stringify(co2Values)}</h1>
-      <p>{co2Values.length}</p>
-      <h1>{JSON.stringify(vocValues)}</h1>
-      <p>{vocValues.length}</p>
-      <h1>{JSON.stringify(timeValues)}</h1>
-      <p>{timeValues.length}</p>
-      <h1>{JSON.stringify(readings)}</h1> */}
-
       <Line data={data} options={options} width={"full"} height={300} />
 
       {/* <Line width={"600px"} height={"600"} data={data} options={options} /> */}
