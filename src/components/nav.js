2a1b5babb86331d3c8dad635439aa5f8be971897
Hasin Mahmood <hasinmahmood01@gmail.com>
2023-01-20 18:15:43 +0000

    updated nav.js

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,10 +1,10 @@
 const Nav = () => {
   return (
     <>
-      <div className="bg-cyan-500 py-1">
-        <div className="flex justify-center">
-          <p className="text-white font-jakarta w-96">breathe.</p>
-        </div>
+      <div className="flex justify-center bg-cyan-500 py-2 ">
+        <p className="text-white font-jakarta w-full px-6 md:max-w-5xl md:text-xl">
+          breathe.
+        </p>
       </div>
     </>
   );
