a1a61c6f32ed6db3db77cb926bbf649cc3a824f5
Hasin Mahmood <hasinmahmood01@gmail.com>
2023-01-20 18:18:09 +0000

    updated tailwind.css config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      maxWidth: {
+        "1000px": "1000px",
+      },
+    },
     fontFamily: {
       jakarta: ["Plus Jakarta Sans", "sans-serif"],
     },
